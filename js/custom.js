21410220199472ecffcaea8984d729faf3e21086
Maroš Kasinec <maros.kasinec@gmail.com>
2013-10-14 16:36:26 +0200

    Added provisionary togglable sidepanels

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,23 @@
 /*jslint browser: true*/
 /*global $, jQuery*/
 
+$.fn.toggleClick = function () {
+    'use strict';
+    var methods = arguments, // store the passed arguments for future reference
+        count = methods.length; // cache the number of methods 
+
+    //use return this to maintain jQuery chainability
+    return this.each(function (i, item) {
+        // for each element you bind to
+        var index = 0; // create a local counter for that element
+        $(item).click(function () { // bind a click handler to that element
+            return methods[index++ % count].apply(this,arguments); // that when called will apply the 'index'th method to that element
+            // the index % count means that we constrain our iterator between 0 and (count-1)
+        });
+    });
+};
+
+
 function sampleRun() {
     'use strict';
     
@@ -31,17 +48,46 @@ $(document).ready(function () {
     $(".gridster > ul").gridster({
         widget_margins: [10, 10],
         widget_base_dimensions: [270, 150]
-        
     });
-    //var gridster = $('.gridster > ul').gridster().data('gridster');
-    /*
-    $('#big-view').click(function () {
-        gridster.resize_widget($('#big-view'), 3, 3);
-    });
-    */
     
     
-    //$('#left-menu').sidr();
+    // sidepanels init
+    //$("#left-menu").css("float", "left").css("background", "whitesmoke").css("overflow", "hidden").css("width", "50px").css("z-index", "1001");
+    
+    
+    initSidepanels();
     
     sampleRun();
 });
+
+function initSidepanels() {
+    var startWidth = 70;
+    var endWidth = 300;
+    
+    $('.sidepanel').width(startWidth);
+    $('.sidepanel li > a > h3').hide();
+    
+    $('.sidepanel-left-trigger').toggleClick(function () {
+        $('.sidepanel-left').animate({
+            width: endWidth
+        }, 0);
+        $('.sidepanel-left li > a > h3').show();
+    }, function () {
+        $(".sidepanel-left").animate({
+            width: startWidth
+        }, 0);
+        $('.sidepanel-left li > a > h3').hide();
+    });
+    
+    $('.sidepanel-right-trigger').toggleClick(function () {
+        $('.sidepanel-right').animate({
+            width: endWidth
+        }, 0);
+        $('.sidepanel-right li > a > h3').show();
+    }, function () {
+        $(".sidepanel-right").animate({
+            width: startWidth
+        }, 0);
+        $('.sidepanel-right li > a > h3').hide();
+    });
+}
