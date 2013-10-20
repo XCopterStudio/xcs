2176532cbdd7c4e83862755d5f900a0c89361851
Michal Koutný <xm.koutny@gmail.com>
2013-10-20 16:47:25 +0200

    typos

--- onboard/src/XCI.hpp
+++ onboard/src/XCI.hpp
@@ -4,25 +4,25 @@
 #include <string>
 #include <vector>
 
-//! Types of senzor which x-copter can posses
-enum SenzorType{
-	AKCELEROMETR, /*!< Akcelerometer senzor. */
-	CAMERA, /*!< Camera senzor. */
-	DISTANCE, /*!< Distance senzor like ultrasonic, laser etc. */  
-	GPS_GALILEO, /*!< Senzor of absolute position on Earth. */
-	GYRO, /*!< Gyroscope senzor. */
-	UNKNOWN /*!< Unknown type of senzor. Use it for new type of senzor which XCS don´t know. */
+//! Types of sensor which x-copter can posses
+enum SensorType{
+	ACCELEROMETR, /*!< Accelerometer sensor. */
+	CAMERA, /*!< Camera sensor. */
+	DISTANCE, /*!< Distance sensor like ultrasonic, laser etc. */  
+	GPS_GALILEO, /*!< Sensor of absolute position on Earth. */
+	GYRO, /*!< Gyroscope sensor. */
+	UNKNOWN /*!< Unknown type of sensor. Use it for new type of sensor which XCS doesn´t know. */
 };
 
-//! Information about senzor
-struct Senzor{
-	std::string name; /*!< Unique name in XCI for senzor. */
-	SenzorType type; /*!< Type of senzor (AKCELEROMETR, CAMERA etc.). */
-	std::string additionalInformation; /*!< Additional information about senzor in plain text. Example: "Resolution:1280x720,FPS:20" */
+//! Information about sensor
+struct Sensor{
+	std::string name; /*!< Unique name in XCI for sensor. */
+	SensorType type; /*!< Type of sensor (ACCELEROMETR, CAMERA etc.). */
+	std::string additionalInformation; /*!< Additional information about sensor in plain text. Example: "Resolution:1280x720,FPS:20" */
 };
 
 // Some usefull typedefs
-typedef std::vector<Senzor> senzorList;
+typedef std::vector<Sensor> sensorList;
 typedef std::vector<std::string> specialCMDList;
 
 //! Virtual class for unified x-copter interface
@@ -30,10 +30,10 @@ class XCI{
 public:
 	//! A pure virtual member returning name of x-copter XCI
 	virtual std::string getName()=0;
-	//! A pure virtual member returning list of available senzor on x-copter
-	virtual senzorList getSenzorList()=0;
-	//! A pure virtual member taking specifikation of senzor and returning void pointer to data from desired senzor
-	virtual void* getSenzorData(Senzor senzor)=0;
+	//! A pure virtual member returning list of available sensors on x-copter
+	virtual sensorList getSensorList()=0;
+	//! A pure virtual member taking specification of sensor and returning void pointer to data from desired sensor
+	virtual void* getSensorData(Sensor sensor)=0;
 	//! A pure virtual member returning x-copter´s configuration
 	virtual std::string getConfiguration()=0;
 	//! A pure virtual member returning list of x-copter´s special commands 
