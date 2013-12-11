f372c4e1477986123e7fe99980d1d4f139202815
ondrap <ondrap@ondra.cz>
2013-12-11 10:14:14 +0100

    Add comments to the tsqueue implementation.

--- shared/tsqueue.hpp
+++ shared/tsqueue.hpp
@@ -6,45 +6,57 @@
 #include <condition_variable>
 #include <memory>
 
+
+//! Queue with support for the thread safe operations.
 template <class T>
 class tsqueue{
-	std::queue<T> queue;
+	std::queue<T> queue; /*!< STL queue for storing a data. */
 
-	mutable std::mutex lock;
-	std::condition_variable emptyCondition;
+	mutable std::mutex lock; /*! Lock which is used for synchronization between access to the STL queue. */
+	std::condition_variable emptyCondition; /*! Wait condition for pop function. */
 public:
+	//! Return true when queue is empty and false otherwise.
 	bool empty() const{ 
-		std::lock_guard<std::mutex> lck(lock);
+		std::lock_guard<std::mutex> lck(lock); //! Acquire lock for safe access to the STL queue
 		return queue.empty();
 	}
 
+	//! Returns pointer to the first element of queue.
+	/*! Function call is blocking. 
+		\return shared pointer to the first element of queue.
+	*/
 	std::shared_ptr<T> pop(){
-		std::unique_lock<std::mutex> lck(lock);
+		std::unique_lock<std::mutex> lck(lock); //! Create unique_lock which we use for waiting on the empty condition.
 
-		emptyCondition.wait(lck,[this]{return !queue.empty();});
+		emptyCondition.wait(lck,[this]{return !queue.empty();}); // Wait until queue is not empty.
 
-		std::shared_ptr<T> pointer = std::shared_ptr<T>(std::make_shared<T>(queue.front()));
-		queue.pop();
+		std::shared_ptr<T> pointer = std::shared_ptr<T>(std::make_shared<T>(queue.front())); // Creates shared pointer to the first element of the queue.
+		queue.pop(); // Pop first element of the queue.
 
 		return pointer;
 	}
 
+	//! Returns pointer to the first element from queue or null pointer if is queue is empty.
+	/*! Function call is not blocking. 
+		\return shared pointer to the first element of queue or null pointer if is queue is empty.
+	*/
 	std::shared_ptr<T> tryPop(){
-		std::lock_guard<std::mutex> lck(lock);
+		std::lock_guard<std::mutex> lck(lock); //! Acquire lock for safe access to the STL queue
 
-		if(queue.empty())
-			return std::shared_ptr<T>();
+		if(queue.empty()) // Test emptiness of the queue.
+			return std::shared_ptr<T>(); // Return null shared pointer.
 
-		std::shared_ptr<T> pointer = std::shared_ptr<T>(std::make_shared<T>(queue.front()));
-		queue.pop();
+		std::shared_ptr<T> pointer = std::shared_ptr<T>(std::make_shared<T>(queue.front())); // Creates shared pointer to the first element of the queue.
+		queue.pop(); // Pop first element of the queue.
 
 		return pointer;
 	};
 
+	//! Push new element to the queue.
 	void push(T value){
-		std::lock_guard<std::mutex> lck(lock);
-		queue.push(value);
-		emptyCondition.notify_one();
+		std::lock_guard<std::mutex> lck(lock); //! Acquire lock for safe access to the STL queue
+		queue.push(value); // Push new element to the queue.
+		emptyCondition.notify_one(); // Wakes up one waiting process on the empty condition.
 	}
 };
 
