abfc7f691f439ebec1f26b2ccccc705aae54a6ed
Michal Koutný <xm.koutny@gmail.com>
2014-01-21 17:53:01 +0100

    experimental NB project + link to Urbi

---
+++ onboard/src/uobjects/Integrator.uob/UIntegrator.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   UIntegrator.cpp
+ * Author: michal
+ * 
+ * Created on January 21, 2014, 5:39 PM
+ */
+
+#include "UIntegrator.hpp"
+
+UIntegrator::UIntegrator() {
+}
+
+UIntegrator::UIntegrator(const UIntegrator& orig) {
+}
+
+UIntegrator::~UIntegrator() {
+}
+
