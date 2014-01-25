25e1472a4907c9d69863034d3f91726bec2a4e2b
Maroš Kasinec <kasinec.maros@gmail.com>
2014-01-25 21:54:23 +0100

    Server: Public served files moved to new 'client' directory structure

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var server = express();
 
-server.use(express.static(__dirname + '/public'));
+// Configuration
+server.set('port', 3000);
+server.set('views', __dirname + '/client/views');
+server.set('view engine', 'hjs');
+
+// Set paths
+server.use('/',    express.static(__dirname + '/client/static'));
+server.use('/css', express.static(__dirname + '/client/css'));
+server.use('/js',  express.static(__dirname + '/client/js'));
 
 server.get('/', function(req, res) {
-    res.render('index.html');
+    res.render('index');
 });
 
-server.listen(3000);
+server.listen(server.get('port'), function () {
+    console.log('XCS node server running on port ' + server.get('port'));
+});
