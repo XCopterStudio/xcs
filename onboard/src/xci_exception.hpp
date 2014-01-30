4792218275392f7da46eadd6334859ae3a2f74ae
ondrap <ondrap@ondrap.cz>
2014-01-30 16:35:40 +0100

    Refactor xci_exception.hpp

--- onboard/src/xci_exception.hpp
+++ onboard/src/xci_exception.hpp
@@ -5,15 +5,15 @@
 #include <string>
 
 class ConnectionErrorException : public std::exception {
-    const std::string message;
+    const std::string message_;
 
 public:
 
-    ConnectionErrorException(const std::string message) : message(message) {
+    ConnectionErrorException(const std::string message) : message_(message) {
     };
 
     const char * what() const throw () {
-        std::string error("Cannot connect to the drone with message: " + message);
+        std::string error("Cannot connect to the drone with message: " + message_);
         return error.c_str();
     }
 };
