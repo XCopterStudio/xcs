f5bf9e0fbb797dfa5a2916bd58342b49954c3bef
Maroš Kasinec <kasinec.maros@gmail.com>
2014-03-02 03:18:20 +0100

    Playing with OpenCV image tranformations and features

--- onboard/src/xcs/urbi/line_finder.uob/u_line_finder.cpp
+++ onboard/src/xcs/urbi/line_finder.uob/u_line_finder.cpp
@@ -20,18 +20,43 @@ void ULineFinder::init() {
         std::cout << "NO IMAGE DATA";
     }*/
 
-    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
-    //cv::imshow("Display Image", image);
-
-    cv::waitKey(20);
+    cv::namedWindow("Source", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("Blured", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("HSV->InRange->Canny", cv::WINDOW_AUTOSIZE);
+    cv::namedWindow("EdgeMap", cv::WINDOW_AUTOSIZE);
 }
 
 
 void ULineFinder::onChangeVideo(::urbi::UVar &uvar) {
     ::urbi::UImage image = uvar;
-    cv::Mat img(image.height, image.width, CV_8UC3, image.data);
+    cv::Mat src(image.height, image.width, CV_8UC3, image.data);
+    cv::Mat mid, dst;
 
-    cv::imshow("Display Image", img);
+    // show original image
+    cv::imshow("Source", src);
+
+    // denoise
+    cv::GaussianBlur(src, mid, cv::Size(5,5), 0, 0);
+    cv::imshow("Blured", mid);
+
+    // convert to HSV
+    cv::cvtColor(mid, mid, CV_BGR2HSV);
+    // highlight ROI (region of interest)
+    // value between 0-40
+    cv::inRange(mid, cv::Scalar(0,0,0), cv::Scalar(255,255,40), mid);
+    // transform to edge map
+    cv::Canny(mid, mid, 200, 200, 3);
+    cv::imshow("HSV->InRange->Canny", mid);
+
+    // detect rectangle
+    cv::vector<cv::Vec4i> lines;
+    cv::HoughLinesP(mid, lines, 1, CV_PI/180, 70, 30, 10);
+//    if (lines.size() > 0)
+//        cv::line(src, cv::Point(lines[0][0], lines[0][1]), cv::Point(lines[0][2], lines[0][3]), cv::Scalar(255,0,0), 2, CV_AA);
+    for (auto l : lines) {
+        cv::line(src, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(255,0,0), 2, CV_AA);
+    }
+    cv::imshow("EdgeMap", src);
 
     cv::waitKey(20);
 }
