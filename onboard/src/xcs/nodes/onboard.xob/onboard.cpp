efed9521af056be2dce8493652365876c11d4912
Maroš Kasinec <kasinec.maros@gmail.com>
2014-03-27 17:55:24 +0100

    Added Onboard XObject for common point of onboard control

---
+++ onboard/src/xcs/nodes/onboard.xob/onboard.cpp
@@ -0,0 +1,51 @@
+#include "onboard.hpp"
+#include <xcs/nodes/xobject/x.hpp>
+#include <boost/log/trivial.hpp>
+
+using namespace xcs::nodes;
+using namespace std;
+
+namespace xcs {
+namespace nodes {
+
+
+Onboard::Onboard(const std::string &name) :
+  xcs::nodes::XObject(name),
+  changeMode("MODE"),
+  mode("MODE"),
+  connection_(0) {
+
+    mode_ = "scripting";
+
+    // TODO: unused so far
+    modes_ = {
+        { "free_flight", Onboard::FREE_FLIGHT },
+        { "assisted_flight", Onboard::ASSISTED_FLIGHT },
+        { "scripting", Onboard::SCRIPTING }
+    };
+
+    XBindVar(mode);
+
+    XBindFunction(Onboard, init);
+    XBindVarF(changeMode, &Onboard::onChangeMode);
+}
+
+void Onboard::init() {
+    // default mode so far, TODO: change to free-flight and run urbiscripts from web GUI
+    mode = "scriptin";
+}
+
+void Onboard::onChangeMode(const string &newMode) {
+    string oldMode = mode_;
+    cerr << "mode_ = " << mode_ << endl;
+    this->send("Onboard.stop_" + oldMode + "();");
+    this->send("Onboard.start_" + newMode + "();");
+    cerr << "[onboard] Mode changed from " << oldMode << " to " << newMode << endl;
+    this->mode = newMode;
+    mode_ = newMode;
+}
+
+}
+}
+
+XStart(Onboard);
