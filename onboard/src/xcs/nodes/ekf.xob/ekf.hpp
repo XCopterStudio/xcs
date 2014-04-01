3e2fb7c01409280e916e9110439265d8cd08ce64
ondrap <ondrap@ondrap.cz>
2014-04-01 10:22:55 +0200

    Expand ekf with the drone state definition.

--- onboard/src/xcs/nodes/ekf.xob/ekf.hpp
+++ onboard/src/xcs/nodes/ekf.xob/ekf.hpp
@@ -1,29 +1,41 @@
 #ifndef EKF_H
 #define EKF_H
 
+#include <queue>
+#include <utility>
+
 #include <xcs/nodes/xci.xob/structs/cartesian_vector.hpp>
 #include <xcs/nodes/xci.xob/structs/eulerian_vector.hpp>
 
 namespace xcs{
-    namespace nodes{
-        namespace ekf{
+namespace nodes{
+namespace ekf{
 
-struct DroneState{
-    ::xcs::nodes::xci::CartesianVector position;
-    ::xcs::nodes::xci::CartesianVector speed;
-    ::xcs::nodes::xci::EulerianVector angles;
-    double angularRotationTheta;
-};
+    struct DroneState{
+        ::xcs::nodes::xci::CartesianVector position;
+        ::xcs::nodes::xci::CartesianVector speed;
+        ::xcs::nodes::xci::EulerianVector angles;
+        double angularRotationTheta;
+    };
 
-class Ekf{
-    DroneState droneState;
-    DroneState droneStateDeviation;
-public:
+    struct DroneStateMeasurement{
+        ::xcs::nodes::xci::CartesianVector speed;
+        ::xcs::nodes::xci::EulerianVector angles;
+        double altitude;
+    };
 
-};
+    // Measurement consist from measurement of the drone state and timestamp. Timestamp is the time when measurement was created.
+    typedef std::queue< std::pair<DroneStateMeasurement, long int> > Measurements; 
+    typedef std::queue< std::pair<DroneState, long int> > DroneStates;
 
-        }
-    }
-}
+    class Ekf{
+        DroneStates droneStates_;
+        DroneStates droneStateDeviation_;
+        Measurements measurements_;
+    public:
+
+    };
+
+}}}
 
 #endif
