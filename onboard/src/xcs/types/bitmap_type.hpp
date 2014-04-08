0ba3173f1bf99d729b7e9c588ab487d6ff743b61
Michal Koutný <xm.koutny@gmail.com>
2014-04-08 17:07:22 +0200

    Added syntactic type for timestamped bitmap

--- onboard/src/xcs/types/bitmap_type.hpp
+++ onboard/src/xcs/types/bitmap_type.hpp
@@ -7,17 +7,31 @@
 namespace xcs {
 
 struct BitmapType {
+    const size_t width;
+    const size_t height;
+    uint8_t * const data;
 
     BitmapType(const size_t width = 0, const size_t height = 0, uint8_t * const data = nullptr) :
       width(width),
       height(height),
       data(data) {
-    };
-
-    const size_t width;
-    const size_t height;
-    uint8_t * const data;
+    }
 };
+
+struct BitmapTypeChronologic : public BitmapType {
+    long int time; // in milliseconds
+
+    BitmapTypeChronologic(const size_t width = 0, const size_t height = 0, uint8_t * const data = nullptr, long int time = -1) :
+      BitmapType(width, height, data),
+      time(time) {
+    }
+
+    BitmapTypeChronologic(const BitmapType &bitmap, long int time = -1) :
+      BitmapType(bitmap), time(time) {
+    }
+
+};
+
 }
 
 #endif	/* BITMAPTYPE_HPP */
