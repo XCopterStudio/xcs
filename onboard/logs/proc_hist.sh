391184df5594541db45b6c80f243f00b0f89e5ef
Michal Koutný <xm.koutny@gmail.com>
2014-04-15 13:28:14 +0200

    Urbiscript for experiments

--- onboard/logs/proc_hist.sh
+++ onboard/logs/proc_hist.sh
@@ -6,8 +6,8 @@
 TMP=tmp.dat
 TMP2=tmp2.dat
 
-grep "$2" "$1" >$TMP
-awk '{print $4,$2,$4-prev; prev=$4}' <$TMP >$TMP2
+cat "$1" > $TMP
+awk '{print $1,$2,$1-prev; prev=$1}' <"$1" >$TMP2
 
 #gnuplot -p -e "set xrange [0:100]; binwidth=1;bin(x,width)=width*floor(x/width);plot \"$TMP2\" using (bin(\$3,binwidth)):(1.0) smooth freq with boxes"
 gnuplot -p -e "set xrange [0:100]; binwidth=1;bin(x,width)=width*floor(x/width);plot \"$TMP2\" using (bin(1000/\$3,binwidth)):(1.0) smooth freq with boxes"
