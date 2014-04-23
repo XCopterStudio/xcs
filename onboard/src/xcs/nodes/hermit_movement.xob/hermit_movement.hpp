576a20c27f467dc226a8cc78fff6d7462f3f8193
ondrap <ondrap@ondra.cz>
2014-04-23 10:24:58 +0200

    CMake: add hermit_movement

---
+++ onboard/src/xcs/nodes/hermit_movement.xob/hermit_movement.hpp
@@ -0,0 +1,15 @@
+#ifndef HERMIT_MOVEMENT_H
+#define HERMIT_MOVEMENT_H
+
+#include <xcs/tsqueue.hpp>
+
+namespace xcs{
+namespace nodes{
+namespace hermit{
+
+class HermitMovement{
+public:
+};
+
+}}}
+#endif
