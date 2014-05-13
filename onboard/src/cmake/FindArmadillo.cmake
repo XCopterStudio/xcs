4436c65cef75bf967bac86bcbf00cf09e88922ff
ondrap <ondrap@ondrap.cz>
2014-05-13 16:22:07 +0200

    Fix: cmake correction for lapack and blas use

--- onboard/src/cmake/FindArmadillo.cmake
+++ onboard/src/cmake/FindArmadillo.cmake
@@ -3,21 +3,23 @@
 #
 
 # Paths
-set(CMAKE_LIBRARY_PATH ${BLAS_ROOT}/lib;${LAPACK_ROOT}/lib;${CMAKE_LIBRARY_PATH})
+set(CMAKE_LIBRARY_PATH ${BLAS_ROOT};${LAPACK_ROOT};${CMAKE_LIBRARY_PATH})
 
 set(Uobject_DIR ${URBI_ROOT}/share/cmake/uobject)
 find_package(Uobject)
 mark_as_advanced(UOBJECT_LIBRARIES)
 
 
-find_library(BLAS_LIBRARIES blas)
+find_library(BLAS_LIBRARIES blas NAMES blas_win64_MT)
+message(${BLAS_LIBRARIES})
 set(BLAS_INCLUDE_DIRS )
 
-find_library(LAPACK_LIBRARIES lapack)
+find_library(LAPACK_LIBRARIES lapack NAMES lapack_win64_MT)
+message(${LAPACK_LIBRARIES})
 set(LAPACK_INCLUDE_DIRS )
 
 set(ARMADILLO_INCLUDE_DIRS ${armadillo_INCLUDE_DIRS};${BLAS_ROOT}/include;${LAPACK_ROOT}/include)
-set(ARMADILLO_LIBRARIES ${blas};${lapack})
+set(ARMADILLO_LIBRARIES ${BLAS_LIBRARIES}; ${LAPACK_LIBRARIES})
 
 
 function(use_armadillo TARGET_NAME)
