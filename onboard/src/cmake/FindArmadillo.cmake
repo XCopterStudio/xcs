065b443442a9ead65b9f7178e10e1abbb26a71d4
Michal Koutný <xm.koutny@gmail.com>
2014-05-27 18:45:00 +0200

    CMake: fixed usage of LAPACK

--- onboard/src/cmake/FindArmadillo.cmake
+++ onboard/src/cmake/FindArmadillo.cmake
@@ -11,11 +11,9 @@ mark_as_advanced(UOBJECT_LIBRARIES)
 
 
 find_library(BLAS_LIBRARIES blas NAMES blas_win64_MT)
-message(${BLAS_LIBRARIES})
 set(BLAS_INCLUDE_DIRS )
 
 find_library(LAPACK_LIBRARIES lapack NAMES lapack_win64_MT)
-message(${LAPACK_LIBRARIES})
 set(LAPACK_INCLUDE_DIRS )
 
 set(ARMADILLO_INCLUDE_DIRS ${armadillo_INCLUDE_DIRS};${BLAS_ROOT}/include;${LAPACK_ROOT}/include)
@@ -25,4 +23,5 @@ set(ARMADILLO_LIBRARIES ${BLAS_LIBRARIES}; ${LAPACK_LIBRARIES})
 function(use_armadillo TARGET_NAME)
 include_directories(${ARMADILLO_INCLUDE_DIRS})
 target_link_libraries(${TARGET_NAME} ${ARMADILLO_LIBRARIES})
+add_definitions("-DARMA_USE_LAPACK -DARMA_USE_BLAS")
 endfunction(use_armadillo)
