42cf455e7e31836fa6a47f49cd8f1eabf910ffce
ondrap <ondrap@ondrap.cz>
2014-06-09 10:04:03 +0200

    Cmake: fix urbi linkage

--- onboard/src/cmake/FindUrbiAll.cmake
+++ onboard/src/cmake/FindUrbiAll.cmake
@@ -22,23 +22,22 @@ set(Serialize_DIR ${URBI_ROOT}/share/cmake/serialize)
 find_package(Serialize)
 mark_as_advanced(SERIALIZE_LIBRARIES)
 
-set(Urbi_DIR ${URBI_ROOT}/share/cmake/urbi)
-find_package(Urbi)
-mark_as_advanced(URBI_LIBRARIES)
-
 set(Uobject_DIR ${URBI_ROOT}/share/cmake/uobject)
 find_package(Uobject)
 mark_as_advanced(UOBJECT_LIBRARIES)
 
-
+set(Urbi_DIR ${URBI_ROOT}/share/cmake/urbi)
+find_package(Urbi)
+mark_as_advanced(URBI_LIBRARIES)
 
 find_package(Boost 1.55 REQUIRED COMPONENTS system)
 
 set(URBIALL_INCLUDE_DIRS ${PORT_INCLUDE_DIRS};${QIJPEG_INCLUDE_DIRS};${SCHED_INCLUDE_DIRS};${SERIALIZE_INCLUDE_DIRS};${UOBJECT_INCLUDE_DIRS};${Boost_INCLUDE_DIRS})
-set(URBIALL_LIBRARIES ${PORT_LIBRARIES};${QIJPEG_LIBRARIES};${SCHED_LIBRARIES};${SERIALIZE_LIBRARIES};${URBI_LIBRARIES};${UOBJECT_LIBRARIES};${Boost_LIBRARIES})
+#set(URBIALL_LIBRARIES ${PORT_LIBRARIES};${QIJPEG_LIBRARIES};${SCHED_LIBRARIES};${SERIALIZE_LIBRARIES};${UOBJECT_LIBRARIES};${Boost_LIBRARIES})
+set(URBIALL_LIBRARIES ${PORT_LIBRARIES};${QIJPEG_LIBRARIES};${SCHED_LIBRARIES};${SERIALIZE_LIBRARIES};${UOBJECT_LIBRARIES};${URBI_LIBRARIES};${Boost_LIBRARIES})
 
 function(use_urbi TARGET_NAME)
 include_directories(${URBIALL_INCLUDE_DIRS})
 target_link_libraries(${TARGET_NAME} ${URBIALL_LIBRARIES})
-# link_directories(${URBI_ROOT}/bin) # I think this is unnecessary. (Michal)
+#link_directories(${URBI_ROOT}/bin) # I think this is unnecessary. (Michal)
 endfunction(use_urbi)
