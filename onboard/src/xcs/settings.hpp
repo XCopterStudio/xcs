67e3b14347933f47f9ba56349437571321e46d84
Michal Koutný <xm.koutny@gmail.com>
2014-06-16 17:40:43 +0200

    Settings: format only

--- onboard/src/xcs/settings.hpp
+++ onboard/src/xcs/settings.hpp
@@ -13,17 +13,21 @@ class XCS_EXPORT Settings {
 public:
     Settings(const std::string& filename = "");
     ~Settings();
+    
     void reset();
     void reset(const std::string& filename);
+    
     template<class Type = std::string>
     Type get(const std::string& key, const Type& defaultValue) const;
     template<class Type = std::string>
-    Type get(const std::string& key) const;    
+    Type get(const std::string& key) const;
+    
     template<class Type = std::string>
     std::map<std::string, Type> getMap(const std::string& key) const;
     template<class Type>
     void set(const std::string& key, const Type& value);
     boost::property_tree::ptree& getTree();
+    
     bool isInit() const;
     bool save();
     bool contains(const std::string& key) const;
@@ -45,7 +49,7 @@ Type Settings::get(const std::string& key) const {
 template<class Type>
 std::map<std::string, Type> Settings::getMap(const std::string& key) const {
     std::map<std::string, Type> m;
-    
+
     // read all settings and fill the map
     if (contains(key)) {
         for (const auto& kvp : settings_.get_child(key)) {
