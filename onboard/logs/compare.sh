926f5cc489bac997367319376661d85686bcf84d
Michal Koutný <xm.koutny@gmail.com>
2014-07-14 10:50:45 +0200

    PTAM: notify with drop (required Urbi patch)

--- onboard/logs/compare.sh
+++ onboard/logs/compare.sh
@@ -5,4 +5,6 @@ cat - >tmp.dat
 grep "(dronetowp)" tmp.dat >pred.dat
 grep "(dronetowr)" tmp.dat >result.dat
 
-gnuplot -p -e 'plot "pred.dat" using ($13):9  t "pred", "result.dat" using ($13):9  t "result"'
+FIELD=$1
+
+gnuplot -p -e 'plot "pred.dat" using ($13):'$FIELD' t "pred", "result.dat" using ($13):'$FIELD'  t "result"'
