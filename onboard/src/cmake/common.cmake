66daa5ca5dab22b528ebb8b91c77bb01991f58b7
Michal Koutný <xm.koutny@gmail.com>
2014-07-16 15:15:59 +0200

    CMake: macro for XObjects, changed binary name (platform independent)

--- onboard/src/cmake/common.cmake
+++ onboard/src/cmake/common.cmake
@@ -4,12 +4,22 @@
 
 function(use_logging TARGET_NAME)
 
-find_package(Boost 1.55 REQUIRED COMPONENTS log)
-include_directories(${Boost_INCLUDE_DIRS})
-target_link_libraries(${TARGET_NAME} ${Boost_LIBRARIES})
-ADD_DEFINITIONS(-DBOOST_LOG_DYN_LINK)
+    find_package(Boost 1.55 REQUIRED COMPONENTS log)
+    include_directories(${Boost_INCLUDE_DIRS})
+    target_link_libraries(${TARGET_NAME} ${Boost_LIBRARIES})
+    ADD_DEFINITIONS(-DBOOST_LOG_DYN_LINK)
 
-find_package (Threads)
-target_link_libraries (${PROJECT_NAME} ${CMAKE_THREAD_LIBS_INIT})
+    find_package (Threads)
+    target_link_libraries (${PROJECT_NAME} ${CMAKE_THREAD_LIBS_INIT})
 
 endfunction(use_logging)
+
+function(add_x_object TARGET_NAME)
+    add_library(${TARGET_NAME} MODULE ${ARGN})
+    set_target_properties(${TARGET_NAME} PROPERTIES PREFIX "")
+
+    x_target_link_libraries(${TARGET_NAME} xobject)
+
+    find_package(UrbiAll)
+    use_urbi(${TARGET_NAME})
+endfunction(add_x_object)
