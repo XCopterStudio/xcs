bfbb52b1e1fb6d5aff54be65368adefa5b484e28
Michal Koutný <xm.koutny@gmail.com>
2014-08-07 01:15:34 +0200

    Console: applied CodeMirror editor

--- server/client/src/views/ConsoleView.js
+++ server/client/src/views/ConsoleView.js
@@ -19,6 +19,22 @@ var ConsoleView = Backbone.View.extend({
         this.$outputElement = this.$('#console-output');
 
         this.onChangeState(this.model);
+
+        this.editor = CodeMirror.fromTextArea(this.$codeElement.get(0), {
+            //theme: "night",
+            mode: "xcs",
+            lineNumbers: true,
+            matchBrackets: true,
+            indentUnit: 2,
+            tabSize: 2,
+            smartIndent: true,
+            electricChars: true,
+            lineWrapping: false,
+            showCursorWhenSelecting: false,
+            undoDepth: 1000,
+            autoFocus: false,
+            //onKeyEvent: function (e, s) { if (s.type == "keyup") { CodeMirror.showHint(e); console.log("test"); } }
+        });
     },
     onClickStart: function() {
         switch (this.model.get('state')) {
@@ -80,7 +96,7 @@ var ConsoleView = Backbone.View.extend({
     },
     onChangeError: function(model) {
         var error = model.get('error');
-        if(!error) {
+        if (!error) {
             this.$outputElement.removeClass('alert-danger');
         } else {
             this.$outputElement.addClass('alert-danger');
