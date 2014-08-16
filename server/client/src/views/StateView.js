71e2ce8213f0f22a5b23f58106c34adb831a5a41
cerpat <cerpat@email.cz>
2014-08-16 18:42:31 +0200

    Prepare server for disconnecting from onboard

--- server/client/src/views/StateView.js
+++ server/client/src/views/StateView.js
@@ -8,5 +8,10 @@ var StateView = Backbone.View.extend({
             //TODO: return context text by x-copter state?
             return "X-copter plan execution will be stopped!"; //Are you sure you want to navigate away?
         }
+        
+        window.onunload = function() {
+            //TODO: send message to onboard?
+            console.log("DISCONNECTING FROM ONBOARD!");
+        }
     },
 });
