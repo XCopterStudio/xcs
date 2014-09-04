d59acbbc77e739507818d998d78f55a2c7ea1d43
Michal Koutný <xm.koutny@gmail.com>
2014-09-04 09:09:38 +0200

    XObject: doxygen

--- onboard/src/xcs/nodes/xobject/x_object.hpp
+++ onboard/src/xcs/nodes/xobject/x_object.hpp
@@ -13,22 +13,65 @@ namespace nodes {
 
 class XOBJECT_EXPORT XObject : public urbi::UObject {
 public:
+
     enum State {
         STATE_CREATED = 0, STATE_STARTED = 1, STATE_STOPPED = 2,
     };
     typedef std::list<std::string> StringList;
+    /*!
+     * \param name This is parameter required by Urbi implementation, your constructor must have same signature and pass this parameter.
+     */
     XObject(const std::string& name);
     virtual ~XObject();
+    /*!
+     * Called to start the dataflow.
+     * 
+     * \note Do not override this method, rather implement xcs::nodes::XObject::stateChanged method.
+     */
     void start();
+    /*!
+     * Called to stop the dataflow.
+     * 
+     * \note Do not override this method, rather implement xcs::nodes::XObject::stateChanged method.
+     */
     void stop();
+    /*!
+     * \return Syntactic type name of XVar/XInputPort with given name.
+     */
     const std::string getSynType(const std::string& xVarName) const;
+    /*!
+     * \return Semantic type name of XVar/XInputPort with given name.
+     */
     const std::string getSemType(const std::string& xVarName) const;
+    /*!
+     * Metadata about the XObject.
+     * 
+     * \return List of names of declared XVars.
+     */
     StringList getXVars() const;
+    /*!
+     * Metadata about the XObject.
+     * 
+     * \return List of names of declared XInputPorts.
+     */
     StringList getXInputPorts() const;
 protected:
+    /*!
+     * It's used to fill XObject metadata.
+     * 
+     * \note It has different usage that xcs::nodes::XDatalogger::registerXVar.
+     */
     bool registerXVar(const std::string& xVarName, const XType& type);
+    /*!
+     * It's used to fill XObject metadata.
+     *      
+     */
     bool registerXInputPort(const std::string& xVarName, const XType& type);
-    // this method will never be called with STATE_CREATED parameter
+    /*!
+     * Override this method to handle XObjects state change.
+     * 
+     * \note This method will never be called with STATE_CREATED parameter.
+     */
     virtual void stateChanged(XObject::State state);
     const XObject::State getState() const;
 private:
